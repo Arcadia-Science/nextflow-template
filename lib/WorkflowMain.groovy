b642ad7b0c242ff90bf2c53e8eaa952cc7627717
Feridun Mert Celebi <mertcelebi93@gmail.com>
2022-10-29 00:22:26 -0400

    Replace all the repo names

--- lib/WorkflowMain.groovy
+++ lib/WorkflowMain.groovy
@@ -1,5 +1,5 @@
 //
-// This file holds several functions specific to the main.nf workflow in the nf-core/nextflowtemplate pipeline
+// This file holds several functions specific to the main.nf workflow in the Arcadia-Science/nextflow-template pipeline
 //
 
 class WorkflowMain {
